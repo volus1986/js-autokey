392691c4799fc4da5558c9e373e01d1f4ce621a3
volus1986 <volus1986@gmail.com>
2022-10-03 21:14:35 +0200

    init

--- src/back-end/main.ts
+++ src/back-end/main.ts
@@ -21,7 +21,7 @@ const createWindow = () => {
 
     // and load the index.html of the app.
     // mainWindow.loadFile(path.join(__dirname, '..', '..', 'public', 'index.html'))
-    mainWindow.loadFile(path.join(__dirname, '..', '..', 'index.html'));
+    mainWindow.loadFile(path.join(__dirname, '..', 'front-end', 'index.html'));
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools()
